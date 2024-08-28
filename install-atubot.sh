3225127ce765fd07eeda9bde8fb504cee83e496a
davidtorres1996 <46732138+davidtorres1996@users.noreply.github.com>
2024-08-27 22:09:26 -0500

    test install

--- install-atubot.sh
+++ install-atubot.sh
@@ -5,38 +5,44 @@ clear
 
 # Actualizar e instalar dependencias
 echo "Actualizando e instalando dependencias..."
-apt update > /dev/null 2>&1
-apt upgrade -y > /dev/null 2>&1
-apt install -y git golang > /dev/null 2>&1
+echo "1/6"
+apt update 
+echo "2/6"
+apt upgrade -y
+echo "3/6"
+apt install -y git golang 
 
 # Clonar el repositorio de WuzAPI desde tu repositorio
 echo "Clonando repositorio..."
-git clone https://github.com/davidtchdev/wuzapi.git > /dev/null 2>&1
+echo "4/6"
+git clone https://github.com/davidtchdev/wuzapi.git 
 
 # Navegar al directorio de WuzAPI
 cd wuzapi
 
 # Instalar dependencias de Go
 echo "Instalando dependencias de Go..."
-go mod tidy > /dev/null 2>&1
+echo "5/6"
+go mod tidy 
 
 echo "Compilando binario..."
+echo "6/6"
 # Verificar si se necesitan permisos de root para compilar
-if ! go build > /dev/null 2>&1; then
+if ! go build 
     echo "Se necesitan permisos de root para compilar. Instalando tsu..."
 
     # Instalar tsu
-    apt install -y tsu > /dev/null 2>&1
+    apt install -y tsu 
 
     # Usar tsu para compilar
-    tsu -c "go build > /dev/null 2>&1"
+    tsu -c "go build"
 else
     # Compilar el proyecto
-    go build > /dev/null 2>&1
+    go build 
 fi
 
 # Configurar permisos si es necesario
-chmod +x ./wuzapi > /dev/null 2>&1
+chmod +x ./wuzapi >
 
 # Ejecutar el binario
 echo "Ejecutando el binario..."
