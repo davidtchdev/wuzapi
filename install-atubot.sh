61a2d3012f8381281d3c5f0b4814857013d5abf8
davidtorres1996 <46732138+davidtorres1996@users.noreply.github.com>
2024-08-28 15:13:06 -0500

    fix installer

--- install-atubot.sh
+++ install-atubot.sh
@@ -11,14 +11,14 @@ apt install -y curl unzip
 
 # Descargar el archivo comprimido desde el repositorio
 echo "Descargando archivo comprimido..."
-curl -L -o wuzapi.zip https://github.com/davidtchdev/wuzapi/releases/download/Assets/atubot_arm64.zip
+curl -L -o atubot_arm64.zip https://github.com/davidtchdev/wuzapi/releases/download/Assets/atubot_arm64.zip
 
 # Descomprimir el archivo
 echo "Descomprimiendo archivo..."
-unzip wuzapi.zip
+unzip atubot_arm64.zip
 
 # Hacer el binario ejecutable
-chmod +x ./wuzapi
+chmod +x ./atubot_arm64
 
 # Ejecutar el binario
-./wuzapi
+./atubot_arm64
