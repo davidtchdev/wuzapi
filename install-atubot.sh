13caf5bd84f71ed2dda3ebf4bd1e10b132cb907e
davidtorres1996 <46732138+davidtorres1996@users.noreply.github.com>
2024-08-27 22:48:12 -0500

    modify sh

--- install-atubot.sh
+++ install-atubot.sh
@@ -7,14 +7,18 @@ clear
 echo "Actualizando e instalando dependencias..."
 apt update
 apt upgrade -y
-apt install -y curl
+apt install -y curl unzip
 
-# Descargar el binario desde el repositorio
-echo "Descargando binario..."
-curl -L -o wuzapi https://github.com/davidtchdev/wuzapi/raw/main/bin/wuzapi
+# Descargar el archivo comprimido desde el repositorio
+echo "Descargando archivo comprimido..."
+curl -L -o wuzapi.zip https://github.com/davidtchdev/wuzapi/releases/download/Assets/wuzapi.zip
+
+# Descomprimir el archivo
+echo "Descomprimiendo archivo..."
+unzip wuzapi.zip
 
 # Hacer el binario ejecutable
-chmod +x wuzapi
+chmod +x ./wuzapi
 
 # Ejecutar el binario
 ./wuzapi
